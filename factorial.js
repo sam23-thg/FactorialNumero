0aac05435379aad938ef45ae7fcb1bc469fadc5e
sam01 <dasalopez23@gmail.com>
2026-09-08 12:20:14 -0600

    eliminando funcion serie factorial

--- factorial.js
+++ factorial.js
@@ -1,11 +1,8 @@
 function factorial(n) {
-    if (n === 0 || n === 1) {
-        return 1;
-    }
+
 
     return n * factorial(n - 1);
 }
 
 console.log(factorial(5)); // 120
-console.log(factorial(55555555555));
-console.log("Hola");
+console.log(factorial(7900000));
