7c25cc7439e7fdd8561a3e22bee6514d8a10e408
sam23-thg <dasalopez23@gmail.com>
2026-09-08 12:38:08 -0600

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -8,3 +8,14 @@ function factorial(n) {
 
 console.log(factorial(5)); // 120
 console.log(factorial(7900000));
+
+function factorial2(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+
+    return n * factorial2(n - 1);
+}
+
+console.log(factorial2(5)); // 120
+console.log(factorial2(7900000));
